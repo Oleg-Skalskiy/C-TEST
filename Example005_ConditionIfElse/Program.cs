0ce9a3c484baaffaf49523fc48421caf11acd455
Oleg_Skalskiy <60063433+Oleg-Skalskiy@users.noreply.github.com>
2023-06-21 22:42:58 +0300

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter User Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "oleg")
+{
+    Console.WriteLine("Yep, this is Oleg");
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.Write(username);
+}
